09a6b6ab7e5c926378cb0e91b1a52e8c129f80ec
Filipe Tartaglia <lipetartaglia9@gmail.com>
2023-11-03 10:32:45 -0300

    Continuando os estudos em logica

---
+++ desafios-js/desafio04/app2.js
@@ -0,0 +1,5 @@
+let valor1 = 25;
+let valor2 = 10;
+let resultado = valor1 - valor2;
+
+console.log(`A diferença de ${valor1} e ${valor2} é igual a ${resultado}.`);
---
+++ desafios-js/desafio04/app4.js
@@ -0,0 +1,11 @@
+let numero = prompt('Escolha um número:');
+
+if (numero > 0) {
+    alert('Seu número é positivo');
+} else {
+    if (numero < 0) {
+        alert('Seu número é negativo');
+    } else {
+        alert('Seu número é zero');
+    }
+}
