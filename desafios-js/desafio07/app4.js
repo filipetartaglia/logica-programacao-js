baaff1ad9d0277fcb894bcc569aa96d7be23ceb0
Filipe Tartaglia <lipetartaglia9@gmail.com>
2023-11-09 14:19:31 -0300

    Desafios de funções concluídos

---
+++ desafios-js/desafio07/app4.js
@@ -0,0 +1,11 @@
+function calcularAreaPerimetroSalaRetangular(altura, largura) {
+    let area = altura * largura; 
+    let perimetro = 2 * (altura + largura);
+
+    console.log(`Área da sala: ${area} metros quadrados`);
+    console.log(`Perímetro da sala: ${perimetro} metros`);
+}
+
+let altura = 3;
+let largura = 5;
+calcularAreaPerimetroSalaRetangular(altura, largura);
---
+++ desafios-js/desafio07/app6.js
@@ -0,0 +1,9 @@
+function mostrarTabuadaDeUmNumero(numero) {
+    for (let i = 1; i <= 10; i++) {
+        let resultado = numero * i;
+        console.log(`${numero} x ${i} = ${resultado}`);
+    }
+}
+
+let numero = 7;
+mostrarTabuadaDeUmNumero(numero);
