33b97b815baf7181e713ab5e1d956278267e86e8
Filipe Tartaglia <lipetartaglia9@gmail.com>
2024-10-29 21:08:09 -0300

    Fazendo projetos para treinar

---
+++ Praticando-logica-projetos/sorteador-numeros/app.js
@@ -0,0 +1,8 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = '';
+
+}
