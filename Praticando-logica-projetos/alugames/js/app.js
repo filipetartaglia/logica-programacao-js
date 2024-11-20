38765a051cf0ac9984857b697c18b71a6e124a26
Filipe Tartaglia <lipetartaglia9@gmail.com>
2024-11-20 19:25:03 -0300

    Terminando projeto carrinho-compras

--- Praticando-logica-projetos/alugames/js/app.js
+++ Praticando-logica-projetos/alugames/js/app.js
@@ -1,16 +1,17 @@
-function alterarStatus(id){
-    let gameClicado = document.getElementById(`game-${id}`);
-    let imagem = gameClicado.querySelector('.dashboard__item__img');
-    let botao = gameClicado.querySelector('.dashboard__item__button');
+function alterarStatus(id) {
+    let jg = document.getElementById(`game-${id}`);
+    let imagem = jg.querySelector('.dashboard__item__img');
+    let botao = jg.querySelector('.dashboard__item__button');
+    let nomeJogo = jg.querySelector('.dashboard__item__name');
 
-    if (imagem.classList.contains('dashboard__item__img--rented')) {
+    if(imagem.classList.contains('dashboard__item__img--rented')) {
         imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return')
         botao.textContent = 'Alugar';
+        botao.classList.remove('dashboard__item__button--return');
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
-        botao.classList.add('dashboard__item__button--return')
-
+        botao.classList.add('dashboard__item__button--return');
     }
+
 }
--- Praticando-logica-projetos/sorteador-numeros/app.js
+++ Praticando-logica-projetos/sorteador-numeros/app.js
@@ -1,8 +1,53 @@
 function sortear() {
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
-    let resultado = document.getElementById('resultado')
-    resultado.innerHTML = '';
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    let resultado = document.getElementById('resultado');
 
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumero(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumero(de, ate);
+        }
+
+        sorteados.push(numero);
+    }
+
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+
+    alterarBotao();
+
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+        return;
+      }
+
+}
+
+function obterNumero(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+
+    }
+}
+
+function reiniciar() {
+     document.getElementById('quantidade').value = '';
+     document.getElementById('de').value = '';
+     document.getElementById('ate').value = '';
+     document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+    alterarBotao();
 }
